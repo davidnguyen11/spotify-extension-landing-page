6048699be241350dcb04df759c9b92f76b3fd36f
davidnguyen179 <dzungnguyen179@gmail.com>
2022-01-29 21:18:47 +0900

    Update number of Edge downloads to 5k+

--- utils/constants.ts
+++ utils/constants.ts
@@ -141,7 +141,7 @@ export const stores: StoreInfo[] = [
     image: '/stores/edge.png',
     title: 'Edge',
     href: 'https://microsoftedge.microsoft.com/addons/detail/spotify-player/odplfjpibjdajlmaocmfmlcdidldlmnk',
-    downloads: '4k+',
+    downloads: '5k+',
   },
 ];
 
