3305aea4c62c0c2d5a11706872f7377147390e46
davidnguyen11 <dzungnguyen179@gmail.com>
2025-04-07 19:21:04 +0900

    Redirect to tuneease

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://www.tunease.com',
+        permanent: true,
+      },
+      {
+        source: '/contact',
+        destination: 'https://www.tunease.com/contact',
+        permanent: true,
+      },
+      {
+        source: '/privacy-policy',
+        destination: 'https://www.tunease.com/privacy-policy',
+        permanent: true,
+      },
+    ]
+  },
 }
